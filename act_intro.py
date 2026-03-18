36553cb431994efbfc94ce7af16819516cade787
pettinellisofia <pettinellisofia06@gmail.com>
2026-03-18 19:47:43 -0300

    1 resuelto

---
+++ act_intro.py
@@ -0,0 +1,5 @@
+anio_nac = int(input("ingresa tu año de nacimiento: "))
+
+print(f"cumpliras 18 años en el año: {anio_nac + 18}")
+print(f"cumpliras 21 años en el año: {anio_nac + 21}")
+print(f"cumpliras 100 años en el año: {anio_nac + 100}")
